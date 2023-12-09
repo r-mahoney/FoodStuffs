32f499c5f8e1d0d434429c76f1f3708d628f3aa3
Ryan Mahoney <rsmahoney211@gmail.com>
2023-12-08 21:22:14 -0600

    updated database env variables

--- packages/db/schema/item.ts
+++ packages/db/schema/item.ts
@@ -3,10 +3,11 @@ import { serial, timestamp, varchar, int } from "drizzle-orm/mysql-core";
 
 import { mySqlTable } from "./_table";
 
-export const item = mySqlTable("post", {
+export const item = mySqlTable("items", {
   id: serial("id").primaryKey(),
   item_name: varchar("item_name", { length: 256 }).notNull(),
   number: int("number").notNull(),
+  units: varchar("units", { length: 256 }),
   createdAt: timestamp("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
